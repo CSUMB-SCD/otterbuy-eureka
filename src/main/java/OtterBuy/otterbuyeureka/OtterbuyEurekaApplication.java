1470adc70d2e8aab61cfb6a93cf8f7cd36606826
Erik Ring-Walters <erik.ring.walters@gmail.com>
2018-12-05 17:15:37 -0800

    creating eureka

---
+++ src/main/java/OtterBuy/otterbuyeureka/OtterbuyEurekaApplication.java
@@ -0,0 +1,12 @@
+package OtterBuy.otterbuyeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtterbuyEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtterbuyEurekaApplication.class, args);
+	}
+}
---
+++ src/test/java/OtterBuy/otterbuyeureka/OtterbuyEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package OtterBuy.otterbuyeureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OtterbuyEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
