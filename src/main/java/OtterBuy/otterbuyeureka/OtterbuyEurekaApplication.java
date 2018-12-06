61498dc0fc4a4f23d3604ce08ab5479945855759
Erik Ring-Walters <erik.ring.walters@gmail.com>
2018-12-05 17:42:29 -0800

    changed app props and configs

--- src/main/java/OtterBuy/otterbuyeureka/OtterbuyEurekaApplication.java
+++ src/main/java/OtterBuy/otterbuyeureka/OtterbuyEurekaApplication.java
@@ -3,6 +3,7 @@ package OtterBuy.otterbuyeureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class OtterbuyEurekaApplication {
 
